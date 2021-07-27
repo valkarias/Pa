1bbc67034618e6f861680c2fe763b33e16240045
valkarias <roofoof500@gmail.com>
2021-07-27 23:30:21 +0200

    :tada: Old VM

--- src/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef pcrap_debug_h
-#define pcrap_debug_h
-
-#include "chunk.h"
-
-void disassembleChunk(Chunk* chunk, const char* name);
-int disassembleInstruction(Chunk* chunk, int offset);
-
-#endif
